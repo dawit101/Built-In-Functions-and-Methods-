a322a3404bd0997c3c22add0858b38a01348a2e1
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-10 00:02:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Built-In-Functions and Methods
+#there are 100s of kinds of built in functions
+
+print(len('hellloooo')) #len = lenght of string, counts starting with 1 not 0
+
+print('------------------')
+
+greet = 'hellloooo'
+print(greet[0:len(greet)]) #answer is hellloooo which counts to the number 9, starting from 1 not 0
+
+print('------------------')
+
+quote = 'to be or not to be'
+
+print(quote.upper()) #it makes all letters capitalize
+
+print(quote.capitalize()) #it capitalize the first letter of sentence
+
+print(quote.replace('be', 'me')) #it changes the first word then adds a new one
